74476e2d7f6c1ecf0e3adb9720f2d0d9a6d24e91
Gabriele Tozzi <tozzigabriele7@gmail.com>
2019-06-04 12:14:57 +0200

    Commit conflitto

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*Gabriele Tozzi 3^AIA 4/05/19 - Verifica GIT*/
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int variabile = 0;
+
+	for (int i = 0; i <= 10; i++) 
+	{
+		variabile++;
+		cout << variabile << endl;
+	}
+
+	system("pause");
+}
